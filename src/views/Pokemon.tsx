e7a297527c899815a680d674e2cc064a5816e4fa
superhan-dev <superhan.dev@gmail.com>
2021-11-04 00:06:58 +0900

    pokemon page code seperation

--- src/views/Pokemon.tsx
+++ src/views/Pokemon.tsx
@@ -1,20 +1,6 @@
 import React from "react";
-import { useGetPokemonByNameQuery } from "../services/pokemon";
+import { PokemonItem } from "../features/pokemon/PokemonItem";
 
 export const Pokemon = () => {
-  const { data, error, isLoading } = useGetPokemonByNameQuery("bulbasaur");
-  return (
-    <div>
-      {error ? (
-        <>Oh no, there was an error</>
-      ) : isLoading ? (
-        <>Loading...</>
-      ) : data ? (
-        <>
-          <h3>{data.species.name}</h3>
-          <img src={data.sprites.front_shiny} alt={data.species.name} />
-        </>
-      ) : null}
-    </div>
-  );
+  return <PokemonItem />;
 };
